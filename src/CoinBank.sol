c4d4455848e693566c60fa468854908f4fb8f581
Damilola Edwards <damilolaedwards@gmail.com>
2024-03-05 15:26:19 +0100

    extended features

--- src/CoinBank.sol
+++ src/CoinBank.sol
@@ -1,30 +1,78 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
+import {IERC20} from "./interfaces/IERC20.sol";
 
 contract CoinBank {
-    address owner;
-    mapping(address => int256) tokenBalancesAndAllowances;
+    address public owner;
+    IERC20 public token;
+    mapping(address => int256) tokenAllowancesAndBalances;
 
-    constructor() {
+    constructor(address _token) {
         owner = msg.sender;
+        token = IERC20(_token);
     }
 
-    function setTokenBalanceAndAllowance(address user, int128 balance, int128 allowance) external {
+    function increaseTokenAllowance(address user, int128 amount, bool increment) external {
         require(msg.sender == owner, "Contract owner only!");
+        
+        //retrieve current allowance
+        (int128 allowance, int128 balance) = getTokenBalanceAndAllowance(user);
+        if(increment){
+            //add increment to previous allowance
+            allowance += amount;
+        }else {
+            //decrement allowance
+            allowance -= amount;
+        }
+       
         // Pack balance and allowance into an int256 variable
-        int256 packedValue = packValues(balance, allowance);
+        int256 packedValue = packValues(allowance, balance);
 
         // Store the packed value in the mapping
-        tokenBalancesAndAllowances[user] = packedValue;
+        tokenAllowancesAndBalances[user] = packedValue;
     }
 
-    function getTokenBalanceAndAllowance(address user) external view returns (int128 balance, int128 allowance) {
+    function deposit(int256 amount) external {
+        require(amount > 0);
+        token.transferFrom(msg.sender, address(this), uint256(amount));
+
+        (int128 allowance, int128 balance) = getTokenBalanceAndAllowance(msg.sender);
+   
+        balance += int128(amount);
+       
+        // Pack balance and allowance into an int256 variable
+        int256 packedValue = packValues(allowance, balance);
+
+        // Store the packed value in the mapping
+        tokenAllowancesAndBalances[msg.sender] = packedValue;
+
+    }
+
+    function withdraw(int256 amount, address receiver) external {
+        require(amount > 0, "amount must be greater than zero");
+        require(address(receiver) != address(0), "receiver cannot be null address");
+        require(token.balanceOf(address(this)) >= uint256(amount), "insufficient balance in contract");
+        (int128 allowance, int128 balance) = getTokenBalanceAndAllowance(msg.sender);
+        int256 withdrawable = allowance + balance;
+        require(amount <= withdrawable, "insufficient user balance");
+        token.transfer(receiver, uint256(amount));
+        balance -= int128(amount);
+        int256 packedValue = packValues(allowance, balance);
+
+        // Store the packed value in the mapping
+        tokenAllowancesAndBalances[msg.sender] = packedValue;
+    }
+
+    function getTokenBalanceAndAllowance(address user) public view returns (int128 allowance, int128 balance) {
         // Retrieve the packed value from the mapping
-        int256 packedValue = tokenBalancesAndAllowances[user];
+        int256 packedValue = tokenAllowancesAndBalances[user];
 
         // Unpack the values from the int256 variable
-        (balance, allowance) = unpackValues(packedValue);
+        (allowance, balance) = unpackValues(packedValue);
     }
+
+    
+    /* ***********INTERNAL FUNCTIONS**********/
 
     // Helper function to pack two int128 values into an int256 variable
     function packValues(int128 value1, int128 value2) internal pure returns (int256) {
